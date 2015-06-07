e5ae5533370f10d53447fc726df91b2ef82db900
Aladdin <alaeddine.messadi@plinga.com>
2015-06-07 12:51:09 +0200

    create, read, update and delete User

---
+++ PngWidget/src/PngWidgetBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PngWidgetBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('hash')
+            ->add('status')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PngWidgetBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pngwidgetbundle_user';
+    }
+}
