6f9f48d308bcf764c33152594b6edbfc1047154f
Aladdin <alaeddine.messadi@plinga.com>
2015-06-07 12:43:45 +0200

    Create PngWidget Bundle

---
+++ PngWidget/src/PngWidgetBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PngWidgetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('PngWidgetBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ PngWidget/src/PngWidgetBundle/PngWidgetBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PngWidgetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PngWidgetBundle extends Bundle
+{
+}
