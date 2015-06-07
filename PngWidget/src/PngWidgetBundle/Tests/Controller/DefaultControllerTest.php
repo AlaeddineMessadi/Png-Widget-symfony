31e4c95d8af164816aefcf7667f819697644b946
Aladdin <alaeddine.messadi@plinga.com>
2015-06-07 18:59:43 +0200

    Add tests UserControllerTest + DefaultControllerTest Fix Edit route

--- PngWidget/src/PngWidgetBundle/Tests/Controller/DefaultControllerTest.php
+++ PngWidget/src/PngWidgetBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,112 @@
 namespace PngWidgetBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PngWidgetBundle\Classes\Color;
+use PngWidgetBundle\Classes\Image;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    //http://127.0.0.1:8000/web/app_dev.php/2d87b0018eaff7a8cf7cf63d5c067e44/w100-h100-bC74343-tECF019
+//        $client = static::createClient();
+//
+//        $crawler = $client->request('GET', '/hello/Fabien');
+//
+//        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        
+    public function testRenderPng()
     {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        // 1d87b0018eaff7a8cf7cf63d5c067e44 Existing User with enabled status 
+        $crawler = $client->request('GET', '/1d87b0018eaff7a8cf7cf63d5c067e44/w100-h100-bC74343-tECF019');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->headers->contains(
+                                                                    'Content-Type',
+                                                                    'image/png'
+                                                                    )
+                         );
     }
+    
+    public function testUserDisabled()
+    {
+        $client = static::createClient();
+        // 9193ce3b31332b03f7d8af056c692b84 Existing User with disabled status 
+        $crawler = $client->request('GET', '/9193ce3b31332b03f7d8af056c692b84/w100-h100-bC74343-tECF019');
+        $this->assertEquals(403,$client->getResponse()->getStatusCode());  
+        $this->assertTrue($client->getResponse()->isForbidden());
+    }
+    
+    public function testUserNotFound()
+    {
+        $client = static::createClient();
+        // 2d87b0018eaff7a8cf7cf63d5c067e44 user not found
+        $crawler = $client->request('GET', '/2d87b0018eaff7a8cf7cf63d5c067e44/w100-h100-bC74343-tECF019');
+        $this->assertEquals(404,$client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isNotFound());
+    }
+    
+    public function testWidthNotValid()
+    {
+        // try width 1000 which is > than 500
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/1d87b0018eaff7a8cf7cf63d5c067e44/w1000-h100-bC74343-tECF019');
+        $this->assertEquals(400,$client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("The Width must be between 100 and 500")')->count()
+        );
+    }
+    
+    public function testHeightNotValid()
+    {
+        // try height 1000 which is > than 500
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/1d87b0018eaff7a8cf7cf63d5c067e44/w100-h1000-bC74343-tECF019');
+        $this->assertEquals(400,$client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("The Height must be between 100 and 500")')->count()
+        );
+    }
+    
+    public function testHexBackgroundColor()
+    {
+        // try height 1000 which is > than 500
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/1d87b0018eaff7a8cf7cf63d5c067e44/w100-h100-bRESDC-tECF019');
+        $this->assertEquals(400,$client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Please check your backgroundcolor Hexa Code")')->count()
+        );
+    }
+    
+    public function testHexTextColor()
+    {
+        // try height 1000 which is > than 500
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/1d87b0018eaff7a8cf7cf63d5c067e44/w100-h100-bC74343-tbest');
+        $this->assertEquals(400,$client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Please check your TextColor Hexa Code")')->count()
+        );
+    }
+    
+    public function testColor() {
+        $im = new Image(100, 100, 'FFF', 'FEF');
+        $bgcolor = new Color($im->getBackgroundColor());
+        $textcolor = new Color($im->getTextColor());
+        
+        $this->assertEquals("FFFFFF", $bgcolor->getRed().$bgcolor->getGreen().$bgcolor->getBlue());
+        $this->assertEquals("FFEEFF", $textcolor->getRed().$textcolor->getGreen().$textcolor->getBlue());
+    }
+    
+    public function testHex() {
+        $color = new Color("FAE");
+        $this->assertEquals("FF", $color->getRed());
+        $this->assertEquals("AA", $color->getGreen());
+        $this->assertEquals("EE", $color->getBlue());
+    }
+    
+    
 }
--- PngWidget/src/PngWidgetBundle/Tests/Controller/UserControllerTest.php
+++ PngWidget/src/PngWidgetBundle/Tests/Controller/UserControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    /*
+    
     public function testCompleteScenario()
     {
         // Create a new client to browse the application
@@ -14,12 +14,12 @@ class UserControllerTest extends WebTestCase
 
         // Create a new entry in the database
         $crawler = $client->request('GET', '/user/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /user/");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'pngwidgetbundle_user[field_name]'  => 'Test',
+            'pngwidgetbundle_user[hash]'  => 'Test',
             // ... other fields to fill
         ));
 
@@ -27,29 +27,18 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        $hash = md5('Test');
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("'.$hash.'")')->count());
 
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'pngwidgetbundle_user[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
+        
+        
+        $crawler = $client->click($crawler->selectLink('edit')->link());
+        var_dump($client->getResponse()->getContent());
         // Delete the entity
         $client->submit($crawler->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
     }
 
-    */
+    
 }
